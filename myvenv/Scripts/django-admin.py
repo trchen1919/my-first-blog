88aafd6ad679a238153f2a636c8d8a18f78bee09
trchen1919 <thomas.chen@girlswhocode.com>
2017-07-25 09:22:09 -0400

    My Django Girls app, first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gwc-nyc-02\desktop\gwc stuff\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
